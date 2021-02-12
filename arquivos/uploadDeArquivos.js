e2c34b0db4b87ada40e0a8c72cddc721152ca54d
Guilherme Mariano Munis <guilherme.munis@pampa.compasso>
2021-02-12 10:54:14 -0300

    Escrevendo Stream de imagens

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,9 +1,5 @@
 const fs = require('fs');
 
-fs.readFile('./assets/ambully.jpg', (erro, buffer) => {
-    console.log('imagem foi bufferizada');
-
-    fs.writeFile('./assets/ambully2.jpg', buffer, erro => {
-        console.log('imagem foi escrita');
-    });
-});
+fs.createReadStream('./assets/ambully.jpg')
+    .pipe(fs.createWriteStream('./assets/ambully-stream.jpg'))
+    .on('finish', () => console.log('Imagem foi escrita com sucesso'))
